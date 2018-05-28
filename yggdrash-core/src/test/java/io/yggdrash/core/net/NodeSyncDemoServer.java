6a7a678dcd56cdc31ccbb3a392103601126f9f61
Luke Hwang <luke@r2v.io>
2018-05-28 11:19:04 +0900

    add pingPong & tests
    - remove grpc-string-boot-starter library

---
+++ yggdrash-core/src/test/java/io/yggdrash/core/net/NodeSyncDemoServer.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright 2018 Akashic Foundation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.yggdrash.core.net;
+
+import java.io.IOException;
+
+/**
+ * The type Node sync demo server.
+ *
+ */
+public class NodeSyncDemoServer {
+    /**
+     * The entry point of application.
+     *
+     * @param args the input arguments
+     * @throws IOException          the io exception
+     * @throws InterruptedException the interrupted exception
+     */
+    public static void main(String[] args) throws IOException, InterruptedException {
+        NodeSyncServer server = new NodeSyncServer(8090);
+        server.start();
+        server.blockUntilShutdown();
+    }
+}
