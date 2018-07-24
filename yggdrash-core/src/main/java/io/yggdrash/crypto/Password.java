cb860e799f1e823750d5f82f15172b0664bfcdd4
jaes <jaes@r2v.io>
2018-07-24 20:37:30 +0900

    Fixed about the password validation.

--- yggdrash-core/src/main/java/io/yggdrash/crypto/Password.java
+++ yggdrash-core/src/main/java/io/yggdrash/crypto/Password.java
@@ -9,7 +9,7 @@ public class Password {
      * Password validation check.
      *
      * @param password password
-     * @return
+     * @return validation
      */
     public static boolean passwordValid(String password) {
         //todo: considering password check lib or NIST regulation
@@ -17,6 +17,12 @@ public class Password {
 
         // password length check
         if (password.length() > 32 || password.length() < 12) {
+            //todo: change from static password length to config properties
+            return false;
+        }
+
+        // check valid character
+        if (!password.matches("[A-Za-z0-9\\x21-\\x2F\\x3A-\\x40\\x5B-\\x60\\x7B-\\x7E]+")) {
             return false;
         }
 
@@ -35,12 +41,8 @@ public class Password {
             return false;
         }
 
-        // 1 more special symbol
-        if (!password.matches("(.*[,~,!,@,#,$,%,^,&,*,(,),-,_,=,+,[,{,],},|,;,:,<,>,/,?].*$)")) {
-            return false;
-        }
-
-        return true;
+        // 1 more special symbol(ASCII character)
+        return password.matches("(.*[\\x21-\\x2F\\x3A-\\x40\\x5B-\\x60\\x7B-\\x7E].*$)");
     }
 
 
@@ -49,7 +51,7 @@ public class Password {
      *
      * @param input     input data
      * @param outLength output length
-     * @return
+     * @return the derivation key by the password
      */
     public static byte[] generateKeyDerivation(byte[] input, int outLength) {
         //todo: checking safety ( IV & ...)
