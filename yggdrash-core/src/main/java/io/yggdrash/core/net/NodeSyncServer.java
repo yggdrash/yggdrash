bc45d92e63df78a2fb1b33e84bd1f0bc48d97bd2
Brian Choi <brian@r2v.io>
2018-07-31 18:14:05 +0900

    Change bidirectional to client2server streaming RPC for tx and block broadcasting

--- yggdrash-core/src/main/java/io/yggdrash/core/net/NodeSyncServer.java
+++ yggdrash-core/src/main/java/io/yggdrash/core/net/NodeSyncServer.java
@@ -40,8 +40,9 @@ import java.util.stream.Collectors;
 
 public class NodeSyncServer {
     private static final Logger log = LoggerFactory.getLogger(NodeSyncServer.class);
-    private final NodeManager nodeManager;
+    private static final BlockChainProto.Empty EMPTY = BlockChainProto.Empty.getDefaultInstance();
     private Server server;
+    private final NodeManager nodeManager;
 
     public NodeSyncServer(NodeManager nodeManager) {
         this.nodeManager = nodeManager;
@@ -92,9 +93,9 @@ public class NodeSyncServer {
      * The block chain rpc server implementation.
      */
     static class BlockChainImpl extends BlockChainGrpc.BlockChainImplBase {
-        private static final Set<StreamObserver<BlockChainProto.Transaction>> txObservers =
+        private static final Set<StreamObserver<BlockChainProto.Empty>> txObservers =
                 ConcurrentHashMap.newKeySet();
-        private static final Set<StreamObserver<BlockChainProto.Block>> blockObservers =
+        private static final Set<StreamObserver<BlockChainProto.Empty>> blockObservers =
                 ConcurrentHashMap.newKeySet();
         private final NodeManager nodeManager;
 
@@ -182,14 +183,14 @@ public class NodeSyncServer {
         public void disconnectPeer(BlockChainProto.PeerRequest peerRequest,
                                    StreamObserver<BlockChainProto.Empty> responseObserver) {
             log.debug("Received disconnect for=" + peerRequest.getFrom());
-            responseObserver.onNext(BlockChainProto.Empty.newBuilder().build());
+            responseObserver.onNext(EMPTY);
             responseObserver.onCompleted();
             nodeManager.removePeer(peerRequest.getFrom());
         }
 
         @Override
         public StreamObserver<BlockChainProto.Transaction> broadcastTransaction(
-                StreamObserver<BlockChainProto.Transaction> responseObserver) {
+                StreamObserver<BlockChainProto.Empty> responseObserver) {
 
             txObservers.add(responseObserver);
 
@@ -210,8 +211,8 @@ public class NodeSyncServer {
                         return;
                     }
 
-                    for (StreamObserver<BlockChainProto.Transaction> observer : txObservers) {
-                        observer.onNext(tx);
+                    for (StreamObserver<BlockChainProto.Empty> observer : txObservers) {
+                        observer.onNext(EMPTY);
                     }
                 }
 
@@ -232,7 +233,7 @@ public class NodeSyncServer {
 
         @Override
         public StreamObserver<BlockChainProto.Block> broadcastBlock(
-                StreamObserver<BlockChainProto.Block> responseObserver) {
+                StreamObserver<BlockChainProto.Empty> responseObserver) {
 
             blockObservers.add(responseObserver);
 
@@ -253,8 +254,8 @@ public class NodeSyncServer {
                         return;
                     }
 
-                    for (StreamObserver<BlockChainProto.Block> observer : blockObservers) {
-                        observer.onNext(protoBlock);
+                    for (StreamObserver<BlockChainProto.Empty> observer : blockObservers) {
+                        observer.onNext(EMPTY);
                     }
                 }
 
--- yggdrash-core/src/test/java/io/yggdrash/core/net/NodeSyncServerTest.java
+++ yggdrash-core/src/test/java/io/yggdrash/core/net/NodeSyncServerTest.java
@@ -49,6 +49,7 @@ import java.util.HashSet;
 import java.util.Set;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
@@ -128,7 +129,7 @@ public class NodeSyncServerTest {
 
         BlockChainGrpc.BlockChainBlockingStub blockingStub
                 = BlockChainGrpc.newBlockingStub(grpcServerRule.getChannel());
-        BlockChainProto.Empty empty = BlockChainProto.Empty.newBuilder().build();
+        BlockChainProto.Empty empty = BlockChainProto.Empty.getDefaultInstance();
         BlockChainProto.TransactionList list = blockingStub.syncTransaction(empty);
         assertEquals(1, list.getTransactionsCount());
     }
@@ -136,30 +137,27 @@ public class NodeSyncServerTest {
     @Test
     public void broadcastTransaction() throws Exception {
         BlockChainGrpc.BlockChainStub stub = BlockChainGrpc.newStub(grpcServerRule.getChannel());
-        StreamRecorder<BlockChainProto.Transaction> responseObserver = StreamRecorder.create();
+        StreamRecorder<BlockChainProto.Empty> responseObserver = StreamRecorder.create();
         StreamObserver<BlockChainProto.Transaction> requestObserver
                 = stub.broadcastTransaction(responseObserver);
 
         BlockChainProto.Transaction request = TransactionMapper.transactionToProtoTransaction(tx);
         requestObserver.onNext(request);
         requestObserver.onCompleted();
-
-        BlockChainProto.Transaction firstTxResponse = responseObserver.firstValue().get();
-        assertEquals("{\"data\":\"TEST\"}", firstTxResponse.getData());
+        assertNotNull(responseObserver.firstValue().get());
     }
 
     @Test
     public void broadcastBlock() throws Exception {
         BlockChainGrpc.BlockChainStub stub = BlockChainGrpc.newStub(grpcServerRule.getChannel());
-        StreamRecorder<BlockChainProto.Block> responseObserver = StreamRecorder.create();
+        StreamRecorder<BlockChainProto.Empty> responseObserver = StreamRecorder.create();
         StreamObserver<BlockChainProto.Block> requestObserver
                 = stub.broadcastBlock(responseObserver);
 
         requestObserver.onNext(BlockMapper.blockToProtoBlock(block));
         requestObserver.onCompleted();
 
-        BlockChainProto.Block firstResponse = responseObserver.firstValue().get();
-        assertEquals(block.getHeader().getTimestamp(), firstResponse.getHeader().getTimestamp());
-        assertEquals("{\"data\":\"TEST\"}", firstResponse.getData().getTrasactions(0).getData());
+        BlockChainProto.Empty firstResponse = responseObserver.firstValue().get();
+        assertNotNull(responseObserver.firstValue().get());
     }
 }
