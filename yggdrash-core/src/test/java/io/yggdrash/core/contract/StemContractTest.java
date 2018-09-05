ef5e888927fc0fab6e902cbac0f425cd916b37c2
Rachael <rachael@r2v.io>
2018-09-05 11:12:17 +0900

    Modify StemContractTest code

--- yggdrash-core/src/test/java/io/yggdrash/core/contract/StemContractTest.java
+++ yggdrash-core/src/test/java/io/yggdrash/core/contract/StemContractTest.java
@@ -138,11 +138,7 @@ public class StemContractTest {
     @Test
     public void getAllBranchNameTest() {
         String branchId = "e1bbdf827bb44f0ae1d88f34e5f3a360484adbf2cf65a6d34162af3bbd4b9523";
-        JsonArray params = new JsonArray();
-        JsonObject param = new JsonObject();
-        param.addProperty("branchId", branchId);
-        params.add(param);
-        assertThat(stemContract.getallbranchname(params)).containsKey(branchId);
+        assertThat(stemContract.getallbranchname(new JsonArray())).containsKey(branchId);
     }
 
     @Test
