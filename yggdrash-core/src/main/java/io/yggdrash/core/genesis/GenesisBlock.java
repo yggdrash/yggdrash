fad4ae4db98d0c760d31d1e752a8243caa94518b
jaes <jaes@r2v.io>
2018-09-07 12:15:21 +0900

    Modified GenesisBlock as data format v0.0.03

--- yggdrash-core/src/main/java/io/yggdrash/core/genesis/GenesisBlock.java
+++ yggdrash-core/src/main/java/io/yggdrash/core/genesis/GenesisBlock.java
@@ -2,24 +2,35 @@ package io.yggdrash.core.genesis;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 import io.yggdrash.config.DefaultConfig;
+import io.yggdrash.core.Block;
+import io.yggdrash.core.BlockBody;
+import io.yggdrash.core.BlockHeader;
 import io.yggdrash.core.BlockHusk;
+import io.yggdrash.core.Transaction;
+import io.yggdrash.core.TransactionBody;
+import io.yggdrash.core.TransactionHeader;
 import io.yggdrash.core.Wallet;
+import io.yggdrash.crypto.HashUtil;
 import io.yggdrash.util.FileUtil;
+import io.yggdrash.util.TimeUtils;
 import org.spongycastle.crypto.InvalidCipherTextException;
 
 import java.io.File;
 import java.io.IOException;
 import java.security.SignatureException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class GenesisBlock {
 
     private final DefaultConfig defaultConfig = new DefaultConfig();
-    private BlockHusk genesisBlock;
+    private Block genesisBlock;
 
-    public GenesisBlock() throws IOException, InvalidCipherTextException {
+    public GenesisBlock() throws IOException, InvalidCipherTextException, SignatureException {
         Wallet wallet = new Wallet(defaultConfig);
 
         String transactionFileName = defaultConfig.getConfig().getString("genesis.config");
@@ -29,11 +40,54 @@ public class GenesisBlock {
         JsonObject frontierObject = getJsonObjectFromFile(frontierFileName);
         genesisObject.add("frontier", frontierObject.get("frontier"));
 
-        String nodeListFileName = defaultConfig.getConfig().getString("genesis.nodeList");
-        JsonObject nodeListObject = getJsonObjectFromFile(nodeListFileName);
-        genesisObject.add("nodeList", nodeListObject.get("nodeList"));
+//        String nodeListFileName = defaultConfig.getConfig().getString("genesis.nodeList");
+//        JsonObject nodeListObject = getJsonObjectFromFile(nodeListFileName);
+//        genesisObject.add("nodeList", nodeListObject.get("nodeList"));
 
-        //genesisBlock = BlockHusk.genesis(wallet, genesisObject);
+        JsonArray jsonArrayTxBody = new JsonArray();
+        jsonArrayTxBody.add(genesisObject);
+
+        TransactionBody txBody = new TransactionBody(jsonArrayTxBody);
+
+        long timestamp = TimeUtils.time();
+
+        // todo: change values(version, type) using the configuration.
+        TransactionHeader txHeader = new TransactionHeader(
+                new byte[20],
+                new byte[8],
+                new byte[8],
+                timestamp,
+                txBody);
+
+        byte[] chain = HashUtil.sha3omit12(txHeader.toBinary());
+
+        // todo: change values(version, type) using the configuration.
+        txHeader = new TransactionHeader(
+                chain,
+                new byte[8],
+                new byte[8],
+                timestamp,
+                txBody);
+
+        Transaction tx = new Transaction(txHeader, wallet, txBody);
+        List<Transaction> txList = new ArrayList<>();
+        txList.add(tx);
+
+        BlockBody blockBody = new BlockBody(txList);
+
+        // todo: change values(version, type) using the configuration.
+        BlockHeader blockHeader = new BlockHeader(
+                chain,
+                new byte[8],
+                new byte[8],
+                new byte[32],
+                0L,
+                timestamp,
+                blockBody.getMerkleRoot(),
+                blockBody.length());
+
+        genesisBlock = new Block(blockHeader, wallet, blockBody);
+
     }
 
     private JsonObject getJsonObjectFromFile(String fileName) throws IOException {
@@ -53,7 +107,7 @@ public class GenesisBlock {
         return new Gson().fromJson(result.toString(), JsonObject.class);
     }
 
-    public BlockHusk getGenesisBlock() {
+    public Block getGenesisBlock() {
         return genesisBlock;
     }
 
