2087a8650a4b94aacf78ba734f0f7a61d43732af
Rachael <rachael@r2v.io>
2018-09-17 12:56:28 +0900

    Check the existence of address which is branchId

--- yggdrash-node/src/main/java/io/yggdrash/node/api/ContractApiImpl.java
+++ yggdrash-node/src/main/java/io/yggdrash/node/api/ContractApiImpl.java
@@ -4,6 +4,7 @@ import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 import com.googlecode.jsonrpc4j.spring.AutoJsonRpcServiceImpl;
 import io.yggdrash.core.BranchGroup;
+import io.yggdrash.core.exception.NonExistObjectException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -26,6 +27,9 @@ public class ContractApiImpl implements ContractApi {
         log.debug("[ContractAPI | data]" + data);
         JsonParser jsonParser = new JsonParser();
         JsonObject query = (JsonObject) jsonParser.parse(data);
+        if(!query.has("address")) {
+           throw new NonExistObjectException("");
+        }
         return branchGroup.query(query).toString();
     }
 }
