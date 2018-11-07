9770d92dae83596ba64aad65cf4e10c403037c32
sarahr2v <sarah@r2v.io>
2018-11-07 11:43:15 +0900

    Fix checkstyle

--- yggdrash-core/src/main/java/io/yggdrash/core/net/PeerGroup.java
+++ yggdrash-core/src/main/java/io/yggdrash/core/net/PeerGroup.java
@@ -28,7 +28,11 @@ import io.yggdrash.proto.Proto;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.stream.Collectors;
 
