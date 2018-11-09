e4702fe0ce9ac7ba006e3cb9d1dc99b210b9b3ef
brian <brian@r2v.io>
2018-11-09 11:45:14 +0900

    Add branch info rest api

--- yggdrash-node/src/main/java/io/yggdrash/node/controller/StateController.java
+++ yggdrash-node/src/main/java/io/yggdrash/node/controller/StateController.java
@@ -16,8 +16,19 @@
 
 package io.yggdrash.node.controller;
 
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import io.yggdrash.core.BlockChain;
+import io.yggdrash.core.BlockHusk;
 import io.yggdrash.core.BranchGroup;
 import io.yggdrash.core.BranchId;
+import io.yggdrash.core.TransactionHusk;
+import io.yggdrash.core.genesis.BranchJson;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -25,6 +36,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -32,16 +44,33 @@ import java.util.Map;
 @RestController
 @RequestMapping("branches")
 public class StateController {
+    private static final Logger log = LoggerFactory.getLogger(StateController.class);
+    private static final ObjectMapper MAPPER = new ObjectMapper();
 
     private final BranchGroup branchGroup;
+
+    static {
+        MAPPER.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+    }
 
     @Autowired
     public StateController(BranchGroup branchGroup) {
         this.branchGroup = branchGroup;
     }
 
+    @GetMapping
+    public ResponseEntity<Map<String, BranchJson>> getBranches() {
+        Map<String, BranchJson> branchMap = new HashMap<>();
+        for (BlockChain branch : branchGroup.getAllBranch()) {
+            BlockHusk genesis = branch.getBlockByIndex(0);
+            BranchJson branchJson = getBranchJson(genesis);
+            branchMap.put(branch.getBranchId().toString(), branchJson);
+        }
+        return ResponseEntity.ok(branchMap);
+    }
+
     @GetMapping("/active")
-    public ResponseEntity getAll() {
+    public ResponseEntity<Map<String, Long>> getAll() {
         Map<String, Long> activeMap = new HashMap<>();
         branchGroup.getAllBranch().forEach(branch ->
                 activeMap.put(branch.getBranchId().toString(), branch.getLastIndex()));
@@ -49,8 +78,35 @@ public class StateController {
     }
 
     @GetMapping("/{branchId}/states")
-    public ResponseEntity getStates(@PathVariable(name = "branchId") String branchId) {
+    public ResponseEntity<List> getStates(@PathVariable(name = "branchId") String branchId) {
         List state = branchGroup.getStateStore(BranchId.of(branchId)).getStateList();
         return ResponseEntity.ok(state);
+    }
+
+    private BranchJson getBranchJson(BlockHusk genesis) {
+        for (TransactionHusk tx : genesis.getBody()) {
+            JsonArray txBody = tx.toJsonObject().getAsJsonArray("body");
+            if (txBody.size() != 0) {
+                return getBranchJson(txBody);
+            }
+        }
+        return new BranchJson();
+    }
+
+    private BranchJson getBranchJson(JsonArray txBody) {
+        try {
+            JsonElement firstTx = txBody.get(0);
+            if (!firstTx.isJsonObject()) {
+                log.warn("Genesis tx is not jsonObject.");
+            } else if (!firstTx.getAsJsonObject().has("branch")) {
+                log.warn("Genesis tx does not contains branch property.");
+            } else {
+                JsonObject branchJson = firstTx.getAsJsonObject().get("branch").getAsJsonObject();
+                return MAPPER.readValue(branchJson.toString(), BranchJson.class);
+            }
+        } catch (IOException e) {
+            log.warn(e.getMessage());
+        }
+        return new BranchJson();
     }
 }
