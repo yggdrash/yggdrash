a76829b8e3267592f45398473e466169114062e6
jaes <jaes@r2v.io>
2018-11-19 11:55:21 +0900

    Fixed a time limit about Transaction verify().
    - added a test code about toStringPretty.

--- yggdrash-core/src/test/java/io/yggdrash/core/TransactionSpeedTest.java
+++ yggdrash-core/src/test/java/io/yggdrash/core/TransactionSpeedTest.java
@@ -163,7 +163,7 @@ public class TransactionSpeedTest {
         log.info(" Transaction:Contructor(byte[]) nanoTime:" + averageTime);
     }
 
-    @Test (timeout = 50000L)
+    @Test (timeout = 5000L)
     public void testSpeedTransactionVerify() {
 
         long startTime;
