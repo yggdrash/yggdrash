5107d031f4782ad5295ea81b6b4b7e4fe198615f
jaes <jaes@r2v.io>
2018-11-20 11:53:29 +0900

    Added assetContract methods about invoke, createDatabase.
    - added a flag for TransactionReceipt.FALSE.
    - added a test method for createDatabase in asset contract.
    - added a brach json for asset management.

--- yggdrash-core/src/main/java/io/yggdrash/core/contract/AssetContract.java
+++ yggdrash-core/src/main/java/io/yggdrash/core/contract/AssetContract.java
@@ -16,12 +16,24 @@
 
 package io.yggdrash.core.contract;
 
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
+import io.yggdrash.common.util.Utils;
 import io.yggdrash.core.TransactionHusk;
+import io.yggdrash.core.exception.FailedOperationException;
 import io.yggdrash.core.store.StateStore;
 import io.yggdrash.core.store.TransactionReceiptStore;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-public class AssetContract extends BaseContract<JsonObject> {
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
+public class AssetContract extends BaseContract<JsonArray> {
+
+    private static final Logger log = LoggerFactory.getLogger(AssetContract.class);
 
     protected TransactionReceiptStore txReceiptStore;
     protected StateStore state;
@@ -33,12 +45,103 @@ public class AssetContract extends BaseContract<JsonObject> {
     }
 
     @Override
-    public boolean invoke(TransactionHusk tx) {
-        return true;
+    public boolean invoke(TransactionHusk txHusk) {
+        TransactionReceipt txReceipt;
+        try {
+            this.sender = txHusk.getAddress().toString();
+            JsonObject txBody = Utils.parseJsonArray(txHusk.getBody()).get(0).getAsJsonObject();
+
+            dataFormatValidation(txBody);
+
+            String method = txBody.get("method").getAsString();
+            JsonArray params = txBody.get("params").getAsJsonArray();
+
+            txReceipt = (TransactionReceipt) this.getClass()
+                    .getMethod(method, JsonArray.class)
+                    .invoke(this, params);
+            txReceipt.putLog("method", method);
+            txReceipt.setTransactionHash(txHusk.getHash().toString());
+            txReceiptStore.put(txReceipt.getTransactionHash(), txReceipt);
+        } catch (Throwable e) {
+            txReceipt = new TransactionReceipt();
+            txReceipt.setTransactionHash(txHusk.getHash().toString());
+            txReceipt.setStatus(0);
+            txReceipt.putLog("Error", e);
+            txReceiptStore.put(txHusk.getHash().toString(), txReceipt);
+        }
+        return txReceipt.isSuccess();
     }
+
+    public TransactionReceipt createDatabase(JsonArray params) {
+        log.debug("createDatabase :: params => " + params);
+
+        TransactionReceipt txReceipt = new TransactionReceipt();
+        txReceipt.setStatus(TransactionReceipt.SUCCESS);
+
+        for (JsonElement element : params) {
+            String dbName = element.getAsJsonObject().get("db").getAsString();
+            if (dbName != null && !dbName.equals("")) {
+                if (state.get(dbName) == null) {
+                    state.put(dbName, new JsonArray());
+                    txReceipt.putLog("createDatabase", dbName);
+                } else {
+                    txReceipt.setStatus(TransactionReceipt.FALSE);
+                    break;
+                }
+            } else {
+                txReceipt.setStatus(TransactionReceipt.FALSE);
+                break;
+            }
+        }
+
+        log.debug(txReceipt.toString());
+
+        return txReceipt;
+    }
+
+    public TransactionReceipt createTable(JsonArray params) {
+        log.debug("createTable :: params => " + params);
+
+        TransactionReceipt txReceipt = new TransactionReceipt();
+        txReceipt.setStatus(TransactionReceipt.SUCCESS);
+
+        for (JsonElement element : params) {
+            String dbName = element.getAsJsonObject().get("db").getAsString();
+            if (dbName != null && !dbName.equals("")) {
+                if (state.get(dbName) == null) {
+                    state.put(dbName, new JsonArray());
+                    txReceipt.putLog("createTable", dbName);
+                } else {
+                    txReceipt.setStatus(TransactionReceipt.FALSE);
+                    break;
+                }
+            } else {
+                txReceipt.setStatus(TransactionReceipt.FALSE);
+                break;
+            }
+        }
+
+        log.debug(txReceipt.toString());
+
+        return txReceipt;
+    }
+
+    private void dataFormatValidation(JsonObject data) {
+        if (data.get("method").getAsString().length() < 0) {
+            throw new FailedOperationException("Empty method");
+        }
+        if (!data.get("params").isJsonArray()) {
+            throw new FailedOperationException("Params must be JsonArray");
+        }
+    }
+
 
     @Override
     public JsonObject query(JsonObject query) {
         return new JsonObject();
     }
+
+
+
+
 }
--- yggdrash-core/src/test/java/io/yggdrash/core/contract/AssetContractTest.java
+++ yggdrash-core/src/test/java/io/yggdrash/core/contract/AssetContractTest.java
@@ -16,16 +16,45 @@
 
 package io.yggdrash.core.contract;
 
+import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
+import io.yggdrash.common.config.DefaultConfig;
+import io.yggdrash.common.util.TimeUtils;
+import io.yggdrash.core.Transaction;
+import io.yggdrash.core.TransactionBody;
+import io.yggdrash.core.TransactionHeader;
+import io.yggdrash.core.TransactionHusk;
+import io.yggdrash.core.TransactionSignature;
+import io.yggdrash.core.account.Wallet;
 import io.yggdrash.core.store.StateStore;
 import io.yggdrash.core.store.TransactionReceiptStore;
 import org.junit.Before;
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.spongycastle.crypto.InvalidCipherTextException;
+import org.spongycastle.util.encoders.Hex;
+
+import java.io.IOException;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 
 public class AssetContractTest {
 
+    private static final Logger log = LoggerFactory.getLogger(AssetContractTest.class);
+
     private AssetContract assetContract;
+
+    private byte[] branchId = Hex.decode("00fbbccfacd572b102351961c048104f4f21a008");
+
+    private DefaultConfig defaultConfig;
+    private Wallet wallet;
+
+    public AssetContractTest() throws IOException, InvalidCipherTextException {
+        defaultConfig = new DefaultConfig();
+        wallet = new Wallet(defaultConfig);
+    }
 
     @Before
     public void setUp() {
@@ -39,7 +68,46 @@ public class AssetContractTest {
     @Test
     public void createDatabaseTest() {
 
+        TransactionBody txBody;
+        TransactionHeader txHeader;
+        TransactionSignature txSig;
+        Transaction tx;
+
+        JsonObject bodyObject = new JsonObject();
+        bodyObject.addProperty("method", "createDatabase");
+
+        JsonObject paramsObject = new JsonObject();
+        paramsObject.addProperty("db", "assetManagement");
+
+        JsonArray paramsArray = new JsonArray();
+        paramsArray.add(paramsObject);
+
+        bodyObject.add("params", paramsArray);
+
+        JsonArray txBodyArray = new JsonArray();
+        txBodyArray.add(bodyObject);
+
+        log.info(txBodyArray.toString());
+
+        txBody = new TransactionBody(txBodyArray);
+        txHeader = new TransactionHeader(
+                branchId,
+                new byte[8],
+                new byte[8],
+                TimeUtils.time(),
+                txBody);
+        txSig = new TransactionSignature(wallet, txHeader.getHashForSigning());
+        tx = new Transaction(txHeader, txSig.getSignature(), txBody);
+
+        boolean result = assetContract.invoke(new TransactionHusk(tx));
+        assertThat(result).isTrue();
     }
+
+
+
+
+
+
 
 }
 
