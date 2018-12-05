5f69e14e67ef8a532b596b62f0b90fec542fa334
jaes <jaes@r2v.io>
2018-12-05 11:22:42 +0900

    Modify aesEncrypt/Decrypt Test() for pbkdf2 algorithm

--- yggdrash-core/src/test/java/io/yggdrash/common/crypto/AESEncryptTest.java
+++ yggdrash-core/src/test/java/io/yggdrash/common/crypto/AESEncryptTest.java
@@ -1,10 +1,13 @@
 package io.yggdrash.common.crypto;
 
+import io.yggdrash.common.util.ByteUtil;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.spongycastle.crypto.InvalidCipherTextException;
 import org.spongycastle.util.encoders.Hex;
+
+import java.security.SecureRandom;
 
 import static org.junit.Assert.assertArrayEquals;
 
@@ -12,24 +15,41 @@ public class AESEncryptTest {
 
     private static final Logger log = LoggerFactory.getLogger(AESEncryptTest.class);
 
+    private static final int WALLET_PBKDF2_ITERATION = 262144;
+    private static final int WALLET_PBKDF2_DKLEN = 32;
+    private static final String WALLET_PBKDF2_ALGORITHM = "SHA-256";
+
     @Test
     public void testEncryptDecrypt1() throws InvalidCipherTextException {
-
-        // password generation using KDF
-        String password = "Aa1234567890#";
-        byte[] kdf = Password.generateKeyDerivation(password.getBytes(), 32);
-
         byte[] plainBytes = "01234567890123450123456789012345345".getBytes();
         log.info("plain: {}", Hex.toHexString(plainBytes));
 
-        byte[] encData = AESEncrypt.encrypt(plainBytes, kdf);
+        String password = "Aa1234567890#";
+        log.info("password: {}", password);
+
+        byte[] iv = new byte[16];
+        byte[] salt = new byte[32];
+        SecureRandom prng = new SecureRandom();
+        prng.nextBytes(iv);
+        prng.nextBytes(salt);
+
+        byte[] kdfPass = HashUtil.pbkdf2(
+                password.getBytes(),
+                salt,
+                WALLET_PBKDF2_ITERATION,
+                WALLET_PBKDF2_DKLEN,
+                WALLET_PBKDF2_ALGORITHM);
+        byte[] encData = AESEncrypt.encrypt(
+                plainBytes,
+                ByteUtil.parseBytes(kdfPass, 0, 16),
+                iv);
         log.info("encrypt: {}", Hex.toHexString(encData));
 
-        byte[] plainData = AESEncrypt.decrypt(encData, kdf);
+        byte[] plainData = AESEncrypt.decrypt(
+                encData, ByteUtil.parseBytes(kdfPass, 0, 16), iv);
         log.info("decrypt: {}", Hex.toHexString(plainData));
 
         assertArrayEquals(plainBytes, plainData);
-
     }
 
     /**
@@ -39,43 +59,37 @@ public class AESEncryptTest {
      */
     @Test
     public void testEncryptDecrypt2() throws InvalidCipherTextException {
-
-        // password generation using KDF
-        String password = "Aa1234567890#";
-        byte[] kdf = Password.generateKeyDerivation(password.getBytes(), 32);
-
         byte[] plain = "0123456789".getBytes();
         byte[] plainBytes = new byte[100000];
+
         for (int i = 0; i < plainBytes.length / plain.length; i++) {
             System.arraycopy(plain, 0, plainBytes, i * plain.length, plain.length);
         }
         log.info("plain: {}", Hex.toHexString(plainBytes));
 
-        byte[] encData = AESEncrypt.encrypt(plainBytes, kdf);
-        log.info("encrypt: {}", Hex.toHexString(encData));
-
-        byte[] plainData = AESEncrypt.decrypt(encData, kdf);
-        log.info("decrypt: {}", Hex.toHexString(plainData));
-
-        assertArrayEquals(plainBytes, plainData);
-
-    }
-
-    @Test
-    public void testEncryptDecrypt3() throws InvalidCipherTextException {
-
-        // password generation using KDF
         String password = "Aa1234567890#";
-        byte[] kdf = new byte[16];
+        log.info("password: {}", password);
+
         byte[] iv = new byte[16];
+        byte[] salt = new byte[32];
+        SecureRandom prng = new SecureRandom();
+        prng.nextBytes(iv);
+        prng.nextBytes(salt);
 
-        byte[] plainBytes = "01234567890123450123456789012345345".getBytes();
-        log.info("plain: {}", Hex.toHexString(plainBytes));
-
-        byte[] encData = AESEncrypt.encrypt(plainBytes, kdf, iv);
+        byte[] kdfPass = HashUtil.pbkdf2(
+                password.getBytes(),
+                salt,
+                WALLET_PBKDF2_ITERATION,
+                WALLET_PBKDF2_DKLEN,
+                WALLET_PBKDF2_ALGORITHM);
+        byte[] encData = AESEncrypt.encrypt(
+                plainBytes,
+                ByteUtil.parseBytes(kdfPass, 0, 16),
+                iv);
         log.info("encrypt: {}", Hex.toHexString(encData));
 
-        byte[] plainData = AESEncrypt.decrypt(encData, kdf, iv);
+        byte[] plainData = AESEncrypt.decrypt(
+                encData, ByteUtil.parseBytes(kdfPass, 0, 16), iv);
         log.info("decrypt: {}", Hex.toHexString(plainData));
 
         assertArrayEquals(plainBytes, plainData);
