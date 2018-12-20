8d7a572f1154892f056e9f5de37841afb75d2f0d
Peter Ryu <peter@yggdrash.io>
2018-12-20 16:51:12 +0900

    Refactor import order

--- yggdrash-gateway/src/main/java/io/yggdrash/gateway/controller/BranchController.java
+++ yggdrash-gateway/src/main/java/io/yggdrash/gateway/controller/BranchController.java
@@ -19,14 +19,14 @@ package io.yggdrash.gateway.controller;
 import io.yggdrash.core.blockchain.BranchGroup;
 import io.yggdrash.core.blockchain.BranchId;
 import io.yggdrash.node.api.dto.BranchDto;
-import java.util.HashMap;
-import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping("branches")
