983761029b1d43099a23aee1a2ffc2e388db4a03
brian <brian@r2v.io>
2019-01-17 15:31:20 +0900

    Change to use PeerTable

--- yggdrash-core/src/main/java/io/yggdrash/core/net/KademliaDiscovery.java
+++ yggdrash-core/src/main/java/io/yggdrash/core/net/KademliaDiscovery.java
@@ -1,57 +1,53 @@
 package io.yggdrash.core.net;
 
-import io.yggdrash.proto.Proto;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 
 public class KademliaDiscovery implements Discovery {
     private static final Logger log = LoggerFactory.getLogger(KademliaDiscovery.class);
 
-    private PeerGroup peerGroup;
+    private PeerTable peerTable;
+    private PeerHandlerFactory factory;
     private Peer owner;
 
-    @Override
-    public void setPeerGroup(PeerGroup peerGroup) {
-        this.peerGroup = peerGroup;
-        this.owner = peerGroup.getOwner();
+    public KademliaDiscovery(PeerTable peerTable) {
+        this.peerTable = peerTable;
+        this.owner = peerTable.getOwner();
     }
 
     @Override
-    public PeerGroup getPeerGroup() {
-        return peerGroup;
-    }
+    public PeerTable discover(PeerHandlerFactory factory) {
+        this.factory = factory;
 
-    @Override
-    public void discover() {
-        for (String ynodeUri : peerGroup.getBootstrappingSeedList()) {
-            String ynodeUriWithoutPubKey = peerGroup.getOwner().getYnodeUri()
+        List<Peer> prevTried = new ArrayList<>();
+        for (String ynodeUri : peerTable.getBootstrappingSeedList()) {
+            String ynodeUriWithoutPubKey = peerTable.getOwner().getYnodeUri()
                     .substring(ynodeUri.indexOf("@"));
             if (ynodeUri.contains(ynodeUriWithoutPubKey)) {
                 continue;
             }
-            Peer peer = Peer.valueOf(ynodeUri);
-            PeerHandler peerHandler = peerGroup.getPeerHandlerFactory().create(peer);
-            log.info("Try connecting to SEED peer = {}", peer);
+            Peer seed = Peer.valueOf(ynodeUri);
+            prevTried.add(seed);
+            PeerHandler peerHandler = factory.create(seed);
+            log.info("Try connecting to SEED peer = {}", seed);
 
             try {
-                Optional<List<String>> list = Optional.ofNullable(
-                        peerHandler.findPeers(owner));
-                list.ifPresent(nodeInfo -> nodeInfo.forEach(
-                        uri -> peerGroup.addPeerByYnodeUri(uri)));
+                List<Peer> peerList = peerHandler.findPeers(owner);
+                peerList.forEach(peerTable::addPeer);
             } catch (Exception e) {
-                log.error("Failed connecting to SEED peer = {}", peer);
+                log.error("Failed connecting to SEED peer = {}", seed);
             } finally {
                 peerHandler.stop();
             }
         }
 
-        int peerCount = peerGroup.count();
-        log.info("Start discover! peerCount={}", peerCount);
-        findPeers(0, new ArrayList<>());
+        int peerCount = peerTable.count();
+        log.info("Start discover! peerCount={}", peerCount - 1);
+        findPeers(0, prevTried);
+        return peerTable;
     }
 
     private synchronized void findPeers(int round, List<Peer> prevTried) {
@@ -60,23 +56,20 @@ public class KademliaDiscovery implements Discovery {
                 log.debug("{}", String.format("(KademliaOptions.MAX_STEPS) Terminating discover"
                         + "after %d rounds.", round));
                 log.trace("{}\n{}",
-                        String.format("Peers discovered %d", peerGroup.count()),
-                        peerGroup.getPeerUriList());
+                        String.format("Peers discovered %d", peerTable.count()),
+                        peerTable.getPeerUriList());
                 return;
             }
 
-            List<Peer> closest = peerGroup.getClosestPeers();
+            List<Peer> closest = peerTable.getClosestPeers();
             List<Peer> tried = new ArrayList<>();
 
             for (Peer peer : closest) {
                 if (!tried.contains(peer) && !prevTried.contains(peer)) {
-                    PeerHandler peerHandler = peerGroup.getPeerHandlerFactory().create(peer);
+                    PeerHandler peerHandler = factory.create(peer);
                     try {
-                        Optional<List<String>> list = Optional.ofNullable(
-                                peerHandler.findPeers(owner));
-                        list.ifPresent(nodeInfo -> nodeInfo.forEach(
-                                uri -> peerGroup.addPeerByYnodeUri(uri)));
-
+                        List<Peer> peerList = peerHandler.findPeers(owner);
+                        peerList.forEach(peerTable::addPeer);
                         tried.add(peer);
                     } catch (Exception e) {
                         log.warn(e.getMessage());
@@ -92,8 +85,8 @@ public class KademliaDiscovery implements Discovery {
             if (tried.isEmpty()) {
                 log.debug("Terminating discover after {} rounds.", round);
                 log.trace("{}\n{}",
-                        String.format("Peers discovered %d", peerGroup.count()),
-                        peerGroup.getPeerUriList());
+                        String.format("Peers discovered %d", peerTable.count()),
+                        peerTable.getPeerUriList());
                 return;
             }
 
--- yggdrash-core/src/test/java/io/yggdrash/core/net/KademliaDiscoveryTest.java
+++ yggdrash-core/src/test/java/io/yggdrash/core/net/KademliaDiscoveryTest.java
@@ -1,20 +1,13 @@
 package io.yggdrash.core.net;
 
-import org.junit.Before;
+import io.yggdrash.PeerTestUtils;
 import org.junit.Test;
 
 public class KademliaDiscoveryTest {
-    private static final Peer OWNER = Peer.valueOf("ynode://75bff16c@127.0.0.1:32918");
-
-    private KademliaDiscovery discovery;
-
-    @Before
-    public void setUp() {
-        this.discovery = new KademliaDiscoveryMock(OWNER);
-    }
 
     @Test
     public void runTest() {
-        discovery.discover();
+        Discovery discovery = new KademliaDiscovery(PeerTestUtils.createPeerTable());
+        discovery.discover(PeerHandlerMock.factory);
     }
 }
