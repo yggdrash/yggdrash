bf31da07d86b038f7fdd82fb4d364b9f93d2f12f
brian <brian@r2v.io>
2019-02-21 13:47:15 +0900

    Add catchUpSync event listener

--- yggdrash-core/src/test/java/io/yggdrash/core/net/BootStrapNodeTest.java
+++ yggdrash-core/src/test/java/io/yggdrash/core/net/BootStrapNodeTest.java
@@ -1,25 +1,31 @@
 package io.yggdrash.core.net;
 
 import io.yggdrash.BlockChainTestUtils;
+import io.yggdrash.TestConstants;
 import io.yggdrash.core.akashic.SimpleSyncManager;
 import io.yggdrash.core.blockchain.BranchGroup;
 import org.junit.Test;
 
-public class BootStrapNodeTest {
+import static org.junit.Assert.assertEquals;
+
+public class BootStrapNodeTest extends BootStrapNode {
+
+    public BootStrapNodeTest() {
+        BranchGroup branchGroup = BlockChainTestUtils.createBranchGroup();
+        setBranchGroup(branchGroup);
+        setNodeStatus(NodeStatusMock.mock);
+        setPeerNetwork(PeerNetworkMock.mock);
+        setSyncManager(new SimpleSyncManager());
+    }
 
     @Test
     public void bootstrappingTest() {
-        YggdrashTestNode node1 = new YggdrashTestNode();
-        node1.bootstrapping();
+        bootstrapping();
     }
 
-    private class YggdrashTestNode extends BootStrapNode {
-        YggdrashTestNode() {
-            BranchGroup branchGroup = BlockChainTestUtils.createBranchGroup();
-            setBranchGroup(branchGroup);
-            setNodeStatus(NodeStatusMock.mock);
-            setPeerNetwork(PeerNetworkMock.mock);
-            setSyncManager(new SimpleSyncManager());
-        }
+    @Test
+    public void catchUpRequestTest() {
+        catchUpRequest(BlockChainTestUtils.createNextBlock());
+        assertEquals(1, branchGroup.getLastIndex(TestConstants.yggdrash()));
     }
 }
