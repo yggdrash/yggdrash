6a19582b4da4b99b1b1c5d50058b05a3359b9970
jaes <jaes@r2v.io>
2019-02-25 16:34:35 +0900

    Add consensus configration in Branch

--- yggdrash-core/src/main/java/io/yggdrash/core/blockchain/Branch.java
+++ yggdrash-core/src/main/java/io/yggdrash/core/blockchain/Branch.java
@@ -42,7 +42,9 @@ public class Branch {
     private final JsonObject json;
     protected List<BranchContract> contracts;
     protected String description;
-    private final List<String> validator;
+    private final List<String> validator; //todo: change to validatorList for integration
+
+    private final JsonObject consensus;
 
     protected Branch(JsonObject json) {
         this.json = json;
@@ -65,6 +67,8 @@ public class Branch {
         this.description = json.get("description").getAsString();
         this.validator = JsonUtil.convertJsonArrayToStringList(
                 json.get("validator").getAsJsonArray());
+
+        consensus = json.get("consensus").getAsJsonObject();
     }
 
     public BranchId getBranchId() {
