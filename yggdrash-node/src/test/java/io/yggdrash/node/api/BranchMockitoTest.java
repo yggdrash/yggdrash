4017a8b1df0022397e03e4b852420cf7d2140af4
jaes <jaes@r2v.io>
2019-02-28 11:21:07 +0900

    Fix checkStyles

--- yggdrash-node/src/test/java/io/yggdrash/node/api/BranchMockitoTest.java
+++ yggdrash-node/src/test/java/io/yggdrash/node/api/BranchMockitoTest.java
@@ -52,28 +52,29 @@ public class BranchMockitoTest {
             String jsonString = JsonUtil.convertObjToString(entry.getValue());
             JsonObject json = JsonUtil.parseJsonObject(jsonString);
 
-            String consensusString = "{\"consensus\": {\n" +
-                    "    \"algorithm\": \"pbft\",\n" +
-                    "    \"period\": \"2\",\n" +
-                    "    \"validator\": {\n" +
-                    "      \"527e5997e79cc0935d9d86a444380a11cdc296b6bcce2c6df5e5439a3cd7bffb945e77aacf881f36a668284984b628063f5d18a214002ac7ad308e04b67bcad8\": {\n" +
-                    "        \"host\": \"127.0.0.1\",\n" +
-                    "        \"port\": \"32911\"\n" +
-                    "      },\n" +
-                    "      \"e12133df65a2e7dec4310f3511b1fa6b35599770e900ffb50f795f2a49d0a22b63e013a393affe971ea4db08cc491118a8a93719c3c1f55f2a12af21886d294d\": {\n" +
-                    "        \"host\": \"127.0.0.1\",\n" +
-                    "        \"port\": \"32912\"\n" +
-                    "      },\n" +
-                    "      \"8d69860332aa6202df489581fd618fc085a6a5af89964d9e556a398d232816c9618fe15e90015d0a2d15037c91587b79465106f145c0f4db6d18b105659d2bc8\": {\n" +
-                    "        \"host\": \"127.0.0.1\",\n" +
-                    "        \"port\": \"32913\"\n" +
-                    "      },\n" +
-                    "      \"b49fbee055a4b3bd2123a60b24f29d69bc0947e45a75eb4880fe9c5b07904c650729e5edcdaff2523c8839889925079963186bd38c22c96433bdbf4465960527\": {\n" +
-                    "        \"host\": \"127.0.0.1\",\n" +
-                    "        \"port\": \"32914\"\n" +
-                    "      }\n" +
-                    "    }\n" +
-                    "  }}";
+            String consensusString = new StringBuilder()
+                    .append("{\"consensus\": {")
+                    .append("    \"algorithm\": \"pbft\",")
+                    .append("    \"period\": \"2\",")
+                    .append("    \"validator\": {")
+                    .append("      \"527e5997e79cc0935d9d86a444380a11cdc296b6bcce2c6df5e5439a3cd7bffb945e77aacf881f36a668284984b628063f5d18a214002ac7ad308e04b67bcad8\": {")
+                    .append("        \"host\": \"127.0.0.1\",")
+                    .append("        \"port\": \"32911\"")
+                    .append("      },")
+                    .append("      \"e12133df65a2e7dec4310f3511b1fa6b35599770e900ffb50f795f2a49d0a22b63e013a393affe971ea4db08cc491118a8a93719c3c1f55f2a12af21886d294d\": {")
+                    .append("        \"host\": \"127.0.0.1\",")
+                    .append("        \"port\": \"32912\"")
+                    .append("      },")
+                    .append("      \"8d69860332aa6202df489581fd618fc085a6a5af89964d9e556a398d232816c9618fe15e90015d0a2d15037c91587b79465106f145c0f4db6d18b105659d2bc8\": {")
+                    .append("        \"host\": \"127.0.0.1\",")
+                    .append("        \"port\": \"32913\"")
+                    .append("      },")
+                    .append("      \"b49fbee055a4b3bd2123a60b24f29d69bc0947e45a75eb4880fe9c5b07904c650729e5edcdaff2523c8839889925079963186bd38c22c96433bdbf4465960527\": {")
+                    .append("        \"host\": \"127.0.0.1\",")
+                    .append("        \"port\": \"32914\"")
+                    .append("      }")
+                    .append("    }")
+                    .append("  }}").toString();
             JsonObject consensus = new Gson().fromJson(consensusString, JsonObject.class);
             json.add("consensus", consensus.get("consensus").getAsJsonObject());
 
