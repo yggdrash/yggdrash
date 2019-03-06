e80b39fa98f9ddcc3597c700136fd3049aac686d
brian <brian@r2v.io>
2019-03-06 13:37:15 +0900

    Apply code inspection

--- yggdrash-core/src/main/java/io/yggdrash/core/blockchain/osgi/ContractCache.java
+++ yggdrash-core/src/main/java/io/yggdrash/core/blockchain/osgi/ContractCache.java
@@ -10,9 +10,12 @@ import org.osgi.framework.launch.Framework;
 import java.lang.annotation.Annotation;
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 class ContractCache {
     //Map<contractName, Map<field, List<Annotation>>>
@@ -24,26 +27,19 @@ class ContractCache {
 
     private final Map<String, Map<String, Method>> endBlockMethods = new HashMap<>();
 
-    ContractCache() {
-        injectingFields = new HashMap<>();
-        invokeTransactionMethods = new HashMap<>();
-        queryMethods = new HashMap<>();
-    }
-
-    public Map<String, Map<Field, List<Annotation>>> getInjectingFields() {
+    Map<String, Map<Field, List<Annotation>>> getInjectingFields() {
         return injectingFields;
     }
 
-
-    public Map<String, Map<String, Method>> getInvokeTransactionMethods() {
+    Map<String, Map<String, Method>> getInvokeTransactionMethods() {
         return invokeTransactionMethods;
     }
 
-    public Map<String, Map<String, Method>> getQueryMethods() {
+    Map<String, Map<String, Method>> getQueryMethods() {
         return queryMethods;
     }
 
-    public Map<String, Map<String, Method>> getEndBlockMethods() {
+    Map<String, Map<String, Method>> getEndBlockMethods() {
         return endBlockMethods;
     }
 
@@ -58,52 +54,32 @@ class ContractCache {
 
         if (injectingFields.get(bundle.getLocation()) == null) {
             Map<Field, List<Annotation>> fields = Arrays.stream(service.getClass().getDeclaredFields())
-                    .filter(field -> {
-                        if (field.getDeclaredAnnotations() != null && field.getDeclaredAnnotations().length > 0) {
-                            return true;
-                        }
-                        return false;
-                    })
+                    .filter(field -> field.getDeclaredAnnotations() != null && field.getDeclaredAnnotations().length > 0)
                     .collect(Collectors.toMap(field -> field, field -> Arrays.asList(field.getDeclaredAnnotations())));
             injectingFields.put(bundle.getLocation(), fields);
         }
 
         if (invokeTransactionMethods.get(bundle.getLocation()) == null) {
             Map<String, Method> methods = Arrays.stream(service.getClass().getDeclaredMethods())
-                    .filter(method -> {
-                        if (method.isAnnotationPresent(InvokeTransaction.class)) {
-                            return true;
-                        }
-                        return false;
-                    })
+                    .filter(method -> method.isAnnotationPresent(InvokeTransaction.class))
                     .filter(method -> Modifier.isPublic(method.getModifiers()))
-                    .collect(Collectors.toMap(m -> m.getName(), m -> m));
+                    .collect(Collectors.toMap(Method::getName, m -> m));
             invokeTransactionMethods.put(bundle.getLocation(), methods);
         }
 
         if (queryMethods.get(bundle.getLocation()) == null) {
             Map<String, Method> methods = Arrays.stream(service.getClass().getDeclaredMethods())
-                    .filter(method -> {
-                        if (method.isAnnotationPresent(ContractQuery.class)) {
-                            return true;
-                        }
-                        return false;
-                    })
+                    .filter(method -> method.isAnnotationPresent(ContractQuery.class))
                     .filter(method -> Modifier.isPublic(method.getModifiers()))
-                    .collect(Collectors.toMap(m -> m.getName(), m -> m));
+                    .collect(Collectors.toMap(Method::getName, m -> m));
             queryMethods.put(bundle.getLocation(), methods);
         }
 
         if (endBlockMethods.get(bundle.getLocation()) == null) {
             Map<String, Method> methods = Arrays.stream(service.getClass().getDeclaredMethods())
-                    .filter(method -> {
-                        if (method.isAnnotationPresent(ContractEndBlock.class)) {
-                            return true;
-                        }
-                        return false;
-                    })
+                    .filter(method -> method.isAnnotationPresent(ContractEndBlock.class))
                     .filter(method -> Modifier.isPublic(method.getModifiers()))
-                    .collect(Collectors.toMap(m -> m.getName(), m -> m));
+                    .collect(Collectors.toMap(Method::getName, m -> m));
             endBlockMethods.put(bundle.getLocation(), methods);
         }
     }
