d6fc8660b1ab6776d26dd077e8717f03b66f4faa
brian <brian@r2v.io>
2019-03-08 16:57:25 +0900

    Add TCP node test

---
+++ yggdrash-node/src/test/java/io/yggdrash/node/TcpNodeTest.java
@@ -0,0 +1,66 @@
+/*
+ * Copyright 2019 Akashic Foundation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.yggdrash.node;
+
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+import io.grpc.Server;
+import io.grpc.ServerBuilder;
+import io.yggdrash.core.p2p.Peer;
+import io.yggdrash.node.service.BlockChainService;
+import io.yggdrash.node.service.DiscoveryService;
+import io.yggdrash.node.springboot.grpc.GrpcServerBuilderConfigurer;
+import io.yggdrash.node.springboot.grpc.GrpcServerRunner;
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.support.GenericApplicationContext;
+
+public class TcpNodeTest extends AbstractNodeTest {
+
+    private final AbstractApplicationContext context = new GenericApplicationContext();
+
+    @Override
+    public void setUp() {
+        super.setUp();
+        context.refresh();
+    }
+
+    @Override
+    protected ManagedChannel createChannel(Peer peer) {
+        return ManagedChannelBuilder.forAddress(peer.getHost(), peer.getPort()).usePlaintext()
+                .build();
+    }
+
+    @Override
+    protected Server createAndStartServer(TestNode node) {
+        GrpcServerBuilderConfigurer configurer = builder -> {
+            builder.addService(new DiscoveryService(node.discoveryConsumer));
+            if (node.blockChainConsumer != null) {
+                builder.addService(new BlockChainService(node.blockChainConsumer));
+            }
+        };
+
+        GrpcServerRunner runner = new GrpcServerRunner(configurer,
+                ServerBuilder.forPort(node.port));
+        runner.setApplicationContext(context);
+        try {
+            runner.run();
+            return runner.getServer();
+        } catch (Exception e) {
+            return null;
+        }
+    }
+}
