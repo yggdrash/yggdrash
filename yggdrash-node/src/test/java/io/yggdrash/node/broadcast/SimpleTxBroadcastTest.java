d86c10832789588398d004eed2fa377c48eec189
brian <brian@r2v.io>
2019-03-13 15:39:21 +0900

    Polish code

--- yggdrash-node/src/test/java/io/yggdrash/node/broadcast/SimpleTxBroadcastTest.java
+++ yggdrash-node/src/test/java/io/yggdrash/node/broadcast/SimpleTxBroadcastTest.java
@@ -29,7 +29,7 @@ import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 
 @RunWith(JUnit4.class)
-public class NetworkBroadcastTest extends AbstractNodeTest {
+public class SimpleTxBroadcastTest extends AbstractNodeTest {
 
     @Test
     public void test() {
