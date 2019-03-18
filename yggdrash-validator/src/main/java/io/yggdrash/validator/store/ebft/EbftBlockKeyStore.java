3eac9619cad2927e57641abac0ded2b66eb853c9
jaes <jaes@r2v.io>
2019-03-18 17:36:55 +0900

    Add locks in validator stores
    - modified size() in store.
    - modified test methods about multi thread stores.
    - Add the PbftBlockStoreMultiThreadTest.

--- yggdrash-validator/src/main/java/io/yggdrash/validator/store/ebft/EbftBlockKeyStore.java
+++ yggdrash-validator/src/main/java/io/yggdrash/validator/store/ebft/EbftBlockKeyStore.java
@@ -2,65 +2,83 @@ package io.yggdrash.validator.store.ebft;
 
 import io.yggdrash.common.store.datasource.DbSource;
 import io.yggdrash.common.utils.ByteUtil;
-import io.yggdrash.core.exception.NonExistObjectException;
+import io.yggdrash.core.exception.NotValidateException;
 import io.yggdrash.validator.store.BlockKeyStore;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.spongycastle.util.encoders.Hex;
 
 import java.io.IOException;
+import java.util.concurrent.locks.ReentrantLock;
 
 public class EbftBlockKeyStore implements BlockKeyStore<Long, byte[]> {
     private static final Logger log = LoggerFactory.getLogger(EbftBlockKeyStore.class);
 
     private final DbSource<byte[], byte[]> db;
+    private long size = 0;
+
+    private final ReentrantLock lock = new ReentrantLock();
 
     public EbftBlockKeyStore(DbSource<byte[], byte[]> dbSource) {
         this.db = dbSource.init();
+        try {
+            this.size = this.db.getAll().size();
+        } catch (IOException e) {
+            log.debug(e.getMessage());
+            throw new NotValidateException("BlockKeyStore is not valid.");
+        }
     }
 
     @Override
     public void put(Long key, byte[] value) {
-        log.trace("EbftBlockKeyStore put "
-                + "(key: " + key + ")"
-                + "(value : " + Hex.toHexString(value) + ")");
-        db.put(ByteUtil.longToBytes(key), value);
+        if (key < 0) {
+            log.debug("Key is not vaild.");
+            return;
+        }
+
+        lock.lock();
+
+        try {
+            if (!contains(key)) {
+                log.trace("put "
+                        + "(key: " + key + ")"
+                        + "(value : " + Hex.toHexString(value) + ")");
+                db.put(ByteUtil.longToBytes(key), value);
+                size++;
+            }
+        } catch (Exception e) {
+            log.debug(e.getMessage());
+        } finally {
+            lock.unlock();
+        }
     }
 
     @Override
     public byte[] get(Long key) {
-        log.trace("EbftBlockKeyStore get " + "(" + key + ")");
-
-        if (key > -1) {
-            byte[] foundValue = db.get(ByteUtil.longToBytes(key));
-            if (foundValue != null) {
-                log.trace("EbftBlockKeyStore value: " + Hex.toHexString(foundValue));
-
-                return foundValue;
-            }
+        if (key < 0) {
+            log.debug("Key is not vaild.");
+            return null;
         }
-        throw new NonExistObjectException("Not Found [" + key + "]");
+
+        log.trace("get " + "(" + key + ")");
+        return db.get(ByteUtil.longToBytes(key));
     }
 
     @Override
     public boolean contains(Long key) {
-        if (key > -1) {
-            return db.get(ByteUtil.longToBytes(key)) != null;
+        if (key < 0) {
+            return false;
         }
 
-        return false;
+        return db.get(ByteUtil.longToBytes(key)) != null;
     }
 
+    @Override
     public long size() {
-        try {
-            return db.getAll().size();
-        } catch (IOException e) {
-            log.error(e.getMessage());
-        }
-
-        return 0;
+        return this.size;
     }
 
+    @Override
     public void close() {
         this.db.close();
     }
