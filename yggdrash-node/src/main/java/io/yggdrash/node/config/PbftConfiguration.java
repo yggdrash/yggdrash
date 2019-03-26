cf5df5107963dcd1c510940fe32ff2fe5bfbab64
brian <brian@r2v.io>
2019-03-26 11:08:12 +0900

    Broadcast block from validator to broadcast-peer-list

--- yggdrash-node/src/main/java/io/yggdrash/node/config/PbftConfiguration.java
+++ yggdrash-node/src/main/java/io/yggdrash/node/config/PbftConfiguration.java
@@ -4,14 +4,27 @@ import io.yggdrash.common.config.DefaultConfig;
 import io.yggdrash.core.blockchain.BlockChain;
 import io.yggdrash.core.blockchain.pbft.PbftBlockChain;
 import io.yggdrash.core.blockchain.pbft.PbftBlockChainBuilder;
+import io.yggdrash.core.net.ValidatorBlockBroadcaster;
 import io.yggdrash.core.p2p.Peer;
+import io.yggdrash.core.p2p.PeerDialer;
+import io.yggdrash.core.p2p.PeerHandlerFactory;
+import io.yggdrash.core.p2p.SimplePeerDialer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Profile("validator")
 @Configuration
 public class PbftConfiguration {
+
+    private final NodeProperties nodeProperties;
+
+    PbftConfiguration(NodeProperties nodeProperties) {
+        this.nodeProperties = nodeProperties;
+    }
 
     @Bean
     PbftBlockChain pbftBlockChain(Peer owner, BlockChain yggdrash, DefaultConfig defaultConfig) {
@@ -20,5 +33,15 @@ public class PbftConfiguration {
                 .setBlockChain(yggdrash)
                 .setConfig(defaultConfig)
                 .build();
+    }
+
+    @Bean
+    ValidatorBlockBroadcaster validatorBlockBroadcaster(BlockChain yggdrash, PeerHandlerFactory peerHandlerFactory) {
+        PeerDialer peerDialer = new SimplePeerDialer(peerHandlerFactory);
+        List<Peer> broadcastPeerList = nodeProperties.getBroadcastPeerList().stream()
+                .map(Peer::valueOf).collect(Collectors.toList());
+        ValidatorBlockBroadcaster broadcaster = new ValidatorBlockBroadcaster(peerDialer, broadcastPeerList);
+        yggdrash.addListener(broadcaster);
+        return broadcaster;
     }
 }
---
+++ yggdrash-node/src/test/java/io/yggdrash/node/broadcast/RestartedServerBroadcastTxTest.java
@@ -0,0 +1,59 @@
+/*
+ * Copyright 2018 Akashic Foundation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.yggdrash.node.broadcast;
+
+import io.yggdrash.TestConstants;
+import io.yggdrash.common.util.Utils;
+import io.yggdrash.node.TcpNodeTesting;
+import io.yggdrash.node.TestNode;
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class RestartedServerBroadcastTxTest extends TcpNodeTesting {
+
+    @Test
+    public void test() {
+        TestConstants.SlowTest.apply();
+        // arrange
+        bootstrapNodes(3, true);
+
+        TestNode server = nodeList.get(1);
+        TestNode client = nodeList.get(2);
+        // add routing to server
+        client.peerTableGroup.addPeer(TestConstants.yggdrash(), server.peerTableGroup.getOwner());
+
+        // act & assert
+        // broadcast success
+        client.generateBlock();
+        Utils.sleep(100);
+        assertThat(server.getDefaultBranch().getLastIndex()).isEqualTo(1);
+
+        // broadcast fail - server shutdown
+        server.shutdown();
+        client.generateBlock();
+        Utils.sleep(100);
+        assertThat(server.getDefaultBranch().getLastIndex()).isEqualTo(1);
+        // broadcast fail - server restart
+        createAndStartServer(server);
+        client.generateBlock();
+        Utils.sleep(100);
+        server.shutdown();
+        // assert
+        assertThat(server.getDefaultBranch().getLastIndex()).isEqualTo(client.getDefaultBranch().getLastIndex());
+    }
+}
