b7e10754199327b170965366feb285b66c01135b
Peter Ryu <peter@yggdrash.io>
2019-05-31 16:10:38 +0900

    Add ContractChannelCoupler Test

--- yggdrash-core/src/test/java/io/yggdrash/core/contract/TestContract.java
+++ yggdrash-core/src/test/java/io/yggdrash/core/contract/TestContract.java
@@ -4,17 +4,28 @@ import com.google.gson.JsonObject;
 import io.yggdrash.common.contract.Contract;
 import io.yggdrash.common.store.StateStore;
 import io.yggdrash.contract.core.TransactionReceipt;
+import io.yggdrash.contract.core.annotation.ContractChannelField;
 import io.yggdrash.contract.core.annotation.ContractQuery;
 import io.yggdrash.contract.core.annotation.ContractStateStore;
 import io.yggdrash.contract.core.annotation.ContractTransactionReceipt;
 import io.yggdrash.contract.core.annotation.InvokeTransaction;
+import io.yggdrash.contract.core.channel.ContractChannel;
+import io.yggdrash.contract.core.channel.ContractMethodType;
 import io.yggdrash.contract.core.store.ReadWriterStore;
+import io.yggdrash.core.blockchain.osgi.ContractManagerBuilderTest;
 import io.yggdrash.core.runtime.annotation.YggdrashContract;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.math.BigInteger;
 
 @YggdrashContract
 public class TestContract implements Contract {
+
+    private static final Logger log = LoggerFactory.getLogger(TestContract.class);
+
+    @ContractChannelField
+    public ContractChannel channel;
 
     @ContractStateStore
     private ReadWriterStore<String, JsonObject> state;
@@ -44,5 +55,33 @@ public class TestContract implements Contract {
     public String someQuery() {
         return "";
     }
+
+    @ContractQuery
+    public String yesmanQuery(JsonObject param) {
+        log.debug("yesmanQuery : {}", param.toString());
+        return "YES";
+    }
+
+    @InvokeTransaction
+    public void callMethod(JsonObject param) {
+        log.debug("callMethod : {}", param.toString());
+    }
+
+    public void callContractChannel(String contract, String method) {
+        JsonObject param = new JsonObject();
+        param.addProperty("contract", contract);
+        param.addProperty("method", method);
+        channel.call(contract, ContractMethodType.INVOKE, method,  param);
+    }
+
+    public void callContractChannelQuery(String contract, String method) {
+        JsonObject param = new JsonObject();
+        param.addProperty("contract", contract);
+        param.addProperty("method", method);
+        JsonObject ressult = channel.call(contract, ContractMethodType.QUERY, method,  param);
+        log.debug(ressult.toString());
+
+    }
+
 
 }
