7e8d8259272cb95dcd92970c94be7eecac64f17a
Lucas <lucas@r2v.io>
2019-08-14 14:17:12 +0900

    Update contract cache, coupler implementation
    update coupler logic
     - change variable contract map to service map
     - coupler can call cached method map from contract cache

--- yggdrash-core/src/main/java/io/yggdrash/core/blockchain/osgi/ContractCache.java
+++ yggdrash-core/src/main/java/io/yggdrash/core/blockchain/osgi/ContractCache.java
@@ -1,6 +1,7 @@
 package io.yggdrash.core.blockchain.osgi;
 
 import io.yggdrash.contract.core.channel.ContractMethodType;
+
 import java.lang.reflect.Method;
 import java.util.Map;
 
@@ -8,4 +9,6 @@ public interface ContractCache {
     void cacheContract(String contractVersion, Object service);
 
     Map<String, Method> getContractMethodMap(String contractVersion, ContractMethodType type);
+
+    Map<String, Method> getContractMethodMap(String contractVersion, ContractMethodType type, Object service);
 }
