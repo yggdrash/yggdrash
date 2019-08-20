610cb31670d8de4c4cf1d8f4118e4e37a9b691cf
kevin <godai77@gmail.com>
2019-08-20 17:05:12 +0900

    TokenContract first commit

---
+++ contracts/token/src/test/java/io/yggdrash/contract/token/TokenContractTest.java
@@ -0,0 +1,28 @@
+package io.yggdrash.contract.token;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class TokenContractTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void start() {
+    }
+
+    @Test
+    public void stop() {
+    }
+
+    @Test
+    public void serviceChanged() {
+    }
+}
