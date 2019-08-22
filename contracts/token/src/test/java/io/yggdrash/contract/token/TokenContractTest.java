074b8cb0de91d40701cda936c984990790675ba2
kevin <godai77@gmail.com>
2019-08-22 18:19:15 +0900

    TokenContract update

--- contracts/token/src/test/java/io/yggdrash/contract/token/TokenContractTest.java
+++ contracts/token/src/test/java/io/yggdrash/contract/token/TokenContractTest.java
@@ -25,4 +25,33 @@ public class TokenContractTest {
     @Test
     public void serviceChanged() {
     }
+
+    private void ref() {
+        TokenContract contract = new TokenContract();
+        TokenContract.TokenService service = new TokenContract.TokenService();
+
+        System.out.println(service.approve(null));
+        System.out.println(service.allowance(null));
+        System.out.println(service.totalSupply(null));
+        System.out.println(service.balanceOf(null));
+        System.out.println(service.burn(null));
+        System.out.println(service.closeExchange(null));
+        System.out.println(service.createToken(null));
+        System.out.println(service.depositYeedStakeToToken(null));
+        System.out.println(service.destroyToken(null));
+        System.out.println(service.exchangeT2T(null));
+        System.out.println(service.exchangeT2Y(null));
+        System.out.println(service.exchangeY2T(null));
+        System.out.println(service.getTokenBalanceOf(null));
+        System.out.println(service.getYeedBalanceOf(null));
+        System.out.println(service.init(null));
+        System.out.println(service.mint(null));
+        System.out.println(service.movePhasePause(null));
+        System.out.println(service.movePhaseRun(null));
+        System.out.println(service.movePhaseStop(null));
+        System.out.println(service.openExchange(null));
+        System.out.println(service.transfer(null));
+        System.out.println(service.transferFrom(null));
+        System.out.println(service.withdrawYeedStakeFromToken(null));
+    }
 }
