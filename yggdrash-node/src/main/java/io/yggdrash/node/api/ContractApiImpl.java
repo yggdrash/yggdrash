35a440c29a5e3244d6003bf22488477355c90337
jaes <jaes@r2v.io>
2019-08-22 17:44:36 +0900

    Fix exceptions logics for stability
    - TODO: check more cases.

--- yggdrash-node/src/main/java/io/yggdrash/node/api/ContractApiImpl.java
+++ yggdrash-node/src/main/java/io/yggdrash/node/api/ContractApiImpl.java
@@ -6,6 +6,8 @@ import com.googlecode.jsonrpc4j.spring.AutoJsonRpcServiceImpl;
 import io.yggdrash.common.utils.JsonUtil;
 import io.yggdrash.core.blockchain.BranchGroup;
 import io.yggdrash.core.blockchain.BranchId;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -18,6 +20,7 @@ import java.util.Set;
 @AutoJsonRpcServiceImpl
 public class ContractApiImpl implements ContractApi {
 
+    private static final Logger log = LoggerFactory.getLogger(ContractApiImpl.class);
     private final BranchGroup branchGroup;
 
     @Autowired
@@ -33,16 +36,21 @@ public class ContractApiImpl implements ContractApi {
             jsonParams = JsonUtil.convertMapToJson(params);
         }
 
-        Object result = branchGroup.query(BranchId.of(branchId), contractVersion, method, jsonParams);
-        if (result instanceof JsonElement) {
-            return JsonUtil.convertJsonToMap((JsonElement)result);
-        } else if (result instanceof BigInteger) {
-            return String.valueOf(result);
-        } else if (result instanceof HashSet) {
-            Set<Map> mapSet = new HashSet<>();
-            Set<JsonElement> obj = (Set<JsonElement>) result;
-            obj.stream().map(JsonUtil::convertJsonToMap).forEach(mapSet::add);
-            return mapSet;
+        Object result = null;
+        try {
+            result = branchGroup.query(BranchId.of(branchId), contractVersion, method, jsonParams);
+            if (result instanceof JsonElement) {
+                return JsonUtil.convertJsonToMap((JsonElement) result);
+            } else if (result instanceof BigInteger) {
+                return String.valueOf(result);
+            } else if (result instanceof HashSet) {
+                Set<Map> mapSet = new HashSet<>();
+                Set<JsonElement> obj = (Set<JsonElement>) result;
+                obj.stream().map(JsonUtil::convertJsonToMap).forEach(mapSet::add);
+                return mapSet;
+            }
+        } catch (Exception e) { // TODO: check more exceptions and logics
+            log.debug("Invalid query branch {}", branchId);
         }
 
         return result;
