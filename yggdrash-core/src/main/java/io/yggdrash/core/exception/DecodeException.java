8b58a84cf369b448bcd96ad941be3882a850a08c
Rachael <rachael@r2v.io>
2019-10-24 21:29:46 +0900

    Upload exception class

---
+++ yggdrash-core/src/main/java/io/yggdrash/core/exception/DecodeException.java
@@ -0,0 +1,37 @@
+package io.yggdrash.core.exception;
+
+import org.apache.commons.codec.DecoderException;
+
+public class DecodeException extends RuntimeException {
+
+    public static final int CODE = -10004;
+
+    public DecodeException(String str) {
+        super("Decode " + str + " failed. " + str + " is not hex string.");
+    }
+
+    public static class TxIdNotHexString extends DecodeException {
+        public TxIdNotHexString() {
+            super("txId");
+        }
+    }
+
+    public static class BranchIdNotHexString extends DecodeException {
+        public BranchIdNotHexString() {
+            super("branchId");
+        }
+    }
+
+    public static class BlockIdNotHexString extends DecodeException {
+        public BlockIdNotHexString() {
+            super("blockId");
+        }
+    }
+
+    public static class ContractVersionNotHexString extends DecodeException {
+        public ContractVersionNotHexString() {
+            super("contractVersion");
+        }
+    }
+
+}
