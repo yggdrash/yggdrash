9de60ab6366d8ef234d981986bdd3f30a7dfd156
jaes <jaes@r2v.io>
2019-11-14 15:42:17 +0900

    Modify a testModule about BranchConfiguration

--- yggdrash-node/src/test/java/io/yggdrash/node/config/BranchConfigurationTest.java
+++ yggdrash-node/src/test/java/io/yggdrash/node/config/BranchConfigurationTest.java
@@ -29,7 +29,6 @@ import io.yggdrash.core.blockchain.genesis.BranchLoader;
 import io.yggdrash.core.consensus.ConsensusBlock;
 import org.apache.commons.io.FileUtils;
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -47,7 +46,6 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
-@Ignore
 public class BranchConfigurationTest {
     private static final Logger log = LoggerFactory.getLogger(BranchConfigurationTest.class);
 
